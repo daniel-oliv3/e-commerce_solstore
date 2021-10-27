c37568e323ba3ec10d09d7d62b51e75d9072360a
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-10-27 20:13:21 -0200

    Update

--- js/app.js
+++ js/app.js
@@ -15,10 +15,11 @@ slide_imgs.forEach((e, index) => {
         displacementImage: 'images/distortion.jpg',
         hover: false
     })
+
     hero_imgs_animate.push(animation)
 })
 
-//remove a imagem
+// remove images
 slide_imgs.forEach(e => e.remove())
 
 let curr_item = 0
@@ -31,10 +32,10 @@ nextSlide = () => {
     let prev_item = curr_item
     curr_item = (curr_item + 1) % hero_imgs_animate.length
 
-    //imagem animation
+    // image animation
     hero_imgs_animate[prev_item].next()
 
-    //change slide info
+    // change slide info
     document.querySelector('.slide-info.active').classList.remove('active')
     slide_infos[curr_item].classList.add('active')
 
@@ -45,9 +46,72 @@ nextSlide = () => {
         document.querySelector('#slide-img').appendChild(canvas[0])
         hero_imgs_animate[prev_item].previous()
     }, 1200);
-
-
-    document.querySelector('#hero-slide-toggle-next').onclick = () => {
-        nextSlide()
-    }
 }
+
+prevSlide = () => {
+    curr_item = curr_item - 1 < 0 ? hero_imgs_animate.length - 1 : curr_item - 1
+
+    // image animation
+    hero_imgs_animate[curr_item].next()
+
+    // change slide info
+    document.querySelector('.slide-info.active').classList.remove('active')
+    slide_infos[curr_item].classList.add('active')
+
+    showSlideIndex(curr_item)
+
+    setTimeout(() => {
+        let canvas = document.querySelectorAll('#slide-img canvas')
+        document.querySelector('#slide-img').insertBefore(canvas[canvas.length - 1], document.querySelector('#slide-img').firstChild)
+        hero_imgs_animate[curr_item].previous()
+    }, 1200);
+}
+
+document.querySelector('#hero-slide-toggle-next').onclick = () => {
+    nextSlide()
+}
+
+document.querySelector('#hero-slide-toggle-prev').onclick = () => {
+    prevSlide()
+}
+
+// auto slide
+
+let auto_slide = setInterval(nextSlide, 5000)
+
+document.querySelector('.hero-section').onmouseover = () => {
+    clearInterval(auto_slide)
+}
+
+document.querySelector('.hero-section').onmouseleave = () => {
+    auto_slide = setInterval(nextSlide, 5000)
+}
+
+document.querySelectorAll('.products-slide').forEach(e => {
+    new Glide(e.querySelector('.glide'), {
+        type: 'slider',
+        starAt: 0,
+        perView: 4,
+        rewind: false,
+        bound: true
+    }).mount()
+})
+
+document.querySelectorAll('.product-img').forEach(e => {
+    e.style.width = e.parentElement.offsetWidth + 'px'
+    e.style.height = e.parentElement.offsetWidth + 'px'
+
+    new hoverEffect({
+        parent: e,
+        intensity: 0.6,
+        image1: e.getAttribute('data-img-1'),
+        image2: e.getAttribute('data-img-2'),
+        displacementImage: 'images/distortion.jpg'
+    })
+})
+
+new Glide('.gallery-slide', {
+    type: 'carousel',
+    starAt: 0,
+    perView: 8,
+}).mount()
